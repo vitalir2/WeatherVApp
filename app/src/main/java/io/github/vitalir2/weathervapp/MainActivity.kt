25c22436180fbc12a44bcf0f590ffffcbca65a49
vitalir2 <vitalyi100@mail.ru>
2021-02-25 17:06:22 +0300

    Initial commit

---
+++ app/src/main/java/io/github/vitalir2/weathervapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.vitalir2.weathervapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
